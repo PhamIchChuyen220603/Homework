e3eb827d4bd7dc606d4fd788b9d8dd0225c841eb
PhamIchChuyen220603 <chuyen.pi11316@sinhvien.hoasen.edu.vn>
2023-02-06 15:51:37 +0700

    D03-TypingPractice

--- index.js
+++ index.js
@@ -3,43 +3,21 @@ function getKey(e) {
     var selector;
     if (location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
         selector = ['[data-key="' + e.keyCode + '-R"]']
+
+        console.log(selector);
     } else {
         var code = e.keyCode || e.which;
         selector = [
             '[data-key="' + code + '"]',
             '[data-char*="' + encodeURIComponent(String.fromCharCode(code)) + '"]'
         ].join(',');
+        console.log(selector);
     }
     return document.querySelector(selector);
 }
 
-function pressKey(char) {
-    var key = document.querySelector('[data-char*="' + char.toUpperCase() + '"]');
-    if (!key) {
-        return console.warn('No key for', char);
-    }
-    key.setAttribute('data-pressed', 'on');
-    setTimeout(function () {
-        key.removeAttribute('data-pressed');
-    }, 200);
-}
 
-var h1 = document.querySelector('h1');
-var originalQueue = h1.innerHTML;
-var queue = h1.innerHTML;
 
-function next() {
-    var c = queue[0];
-    queue = queue.slice(1);
-    h1.innerHTML = originalQueue.slice(0, originalQueue.length - queue.length);
-    pressKey(c);
-    if (queue.length) {
-        setTimeout(next, Math.random() * 200 + 50);
-    }
-}
-
-h1.innerHTML = "&nbsp;";
-setTimeout(next, 500);
 
 document.body.addEventListener('keydown', function (e) {
     var key = getKey(e);
@@ -54,6 +32,7 @@ document.body.addEventListener('keyup', function (e) {
     var key = getKey(e);
     key && key.removeAttribute('data-pressed');
 });
+
 
 function size() {
     var size = keyboard.parentNode.clientWidth / 90;
